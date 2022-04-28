97198c255eeb56895d5916585cd1bebe11b763cf
Corey Schadeck <corey@schadeck.com>
2022-04-28 13:38:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// 1  array for referance = const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (driver, name) => {
+    let result = driver.filter((element) => element.toLowerCase() === name.toLowerCase() );
+    return result
+}
+    //return driver.toLowerCase().indexOf(query.toLowerCase()) !== -1
+
+
+function fuzzyMatch (drivers, string){
+    return drivers.filter((element) => element.charAt(0) === string.charAt(0))
+}
+    function matchName( drivers, cat) {
+        return drivers.filter((element) => element.name === cat)
+    }
+
+
+  
